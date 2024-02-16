90789ab24c64784bf47dc23691050e0d21d826ce
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-02-16 15:37:57 -0300

    feat: remove routing from server file

--- src/server.js
+++ src/server.js
@@ -1,39 +1,19 @@
 import http from "node:http";
-
-const users = [];
+import { json } from "./middlewares/json.js";
+import { routes } from "./routes.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  const buffers = [];
+  await json(req, res);
 
-  for await (const chunk of req) {
-    buffers.push(chunk);
+  const route = routes[method][url];
+
+  if (route) {
+    return route(req, res);
   }
 
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch {
-    req.body = null;
-  }
-
-  if (method === "GET" && url === "/users") {
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users));
-  }
-
-  if (method === "POST" && url === "/users") {
-    const { name, email } = req.body;
-
-    users.push({
-      id: 1,
-      name,
-      email,
-    });
-
-    return res.writeHead(201).end();
-  }
   return res.writeHead(404).end();
 });
+
 server.listen(3333);
