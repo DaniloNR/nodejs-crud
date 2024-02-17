f26149124f49a9a242dd4fb3a8669aea409a006c
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-02-17 15:53:19 -0300

    feat: add search query on list route

--- src/database.js
+++ src/database.js
@@ -11,8 +11,18 @@ export class Database {
       .catch(this.#persist.bind(this));
   }
 
-  select(table) {
-    return this.#database[table] ?? [];
+  select(table, search) {
+    let data = this.#database[table] ?? [];
+
+    if (search) {
+      data = data.filter((row) => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase());
+        });
+      });
+    }
+
+    return data;
   }
 
   insert(table, data) {
